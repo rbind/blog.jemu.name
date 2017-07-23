86491eb5ffbbc9202e793981590aa17e46be82f5
jemus42 <jemus42@quantenbrot.de>
2017-07-23 22:29:19 +0200

    Set ggplot theme

--- global.R
+++ global.R
@@ -5,3 +5,5 @@ knitr::opts_chunk$set(out.width = "90%",
                       fig.retina = 2,
                       warning = F,
                       message = F)
+
+ggplot2::theme_set(tadaatoolbox::theme_tadaa())
