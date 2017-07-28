8b6614f9150bddad7cb2a61e833cf0d2ccee00cf
jemus42 <jemus42@quantenbrot.de>
2017-07-29 00:40:15 +0200

    A bit of cleanup & refactor(ish)ing

--- build.sh
+++ build.sh
@@ -4,8 +4,16 @@
 
 Rscript -e 'blogdown::build_site()'
 
+echo "----------------"
+echo "Optimizing images..."
+find public/post -name "*.png" |xargs optipng
+
+echo "----------------"
+echo "Syncing public/ to server..."
+find . -name '.DS_Store' -delete
 rsync -avz --delete public/ mercy:/srv/blog.jemu.name/public/
 
 echo "----------------"
 echo "Done with things"
 date +"%F %H:%M:%S"
+
--- global.R
+++ global.R
@@ -5,11 +5,15 @@ library(dplyr)
 library(knitr)
 library(tadaatoolbox)
 
+# Hugo config
+config_toml <- RcppTOML::parseTOML("config.toml")
+
 # Global chunk options
-knitr::opts_chunk$set(out.width = "90%",
+knitr::opts_chunk$set(out.width = "100%",
                       fig.retina = 2,
                       warning = F,
-                      message = F)
+                      message = F,
+                      comment = "")
 
 ggplot2::theme_set(tadaatoolbox::theme_tadaa())
 
@@ -23,25 +27,25 @@ hook <- function(x, options) {
     height <- sprintf(' height = "%s" ', options$out.height)
 
   filename <- paste0("../../../post/", knitr::opts_knit$get('base.url'), paste(x, collapse = '.'))
+  basename <- stringr::str_replace(x, ".*\\/", "")
+  id       <- stringr::str_extract(x, "^[^\\/]*")
 
   if (!is.null(options$fig.cap)) {
-    caption <- paste0("<figcaption>", options$fig.cap, "</figcaption>")
+    caption <- options$fig.cap
   } else {
-    caption <- NULL
+    caption <- opts_current$get("label")
   }
-  plot_id <- paste0(paste0(sample(c(letters, LETTERS),
-                                  size = 5, replace = T), collapse = ""),
-                    caption)
 
-  img <- paste0("<img src='", filename, "' ", width, height, " alt='' />")
-  lightbox <- paste0("<a href='", filename, "' ",
-                     "data-lightbox='", plot_id, "' data-title='", plot_id, "'>",
-                     img, "</a>")
+  img     <- paste0("<img src='", filename, "'", width, height, "alt='' />")
+  imglink <- paste0("<a href='", filename, "' ",
+                     "class = 'swipebox fresco' ",
+                     "data-lightbox='", id, "' ",
+                     "data-title='", caption, "' ",
+                     "data-fresco-caption='", caption, "' ",
+                     "data-fresco-group='", id, "' ",
+                     ">", img, "</a>")
 
-  figure <- paste0("<figure>", lightbox, caption, "</figure>")
-
-  return(figure)
-
+  paste0("<figure>", imglink, "<figcaption>", caption, "</figcaption></figure>")
 }
 
 knitr::knit_hooks$set(plot = hook)
