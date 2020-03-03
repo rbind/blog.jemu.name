484e239e63a8b933f5ed8bd424ac857eedfe6dea
Lukas Burk <lukas@quantenbrot.de>
2020-03-03 10:18:00 +0100

    Trying to find out what git-auto-deploy sees

--- build.sh
+++ build.sh
@@ -2,16 +2,29 @@
 
 # rm -r public
 
+# Debugging
+echo which pandoc
+which pandoc
+echo where pandoc
+where pandoc
+echo pandoc version
+pandoc --version
+
+echo "##########################"
+echo "# Restoring renv library #"
+echo "##########################"
 Rscript -e 'renv::restore()'
+
+echo "##########################"
+echo "# Installing Hugo        #"
+echo "##########################"
 Rscript -e 'blogdown::install_hugo(version = "0.65.3")'
+
+echo "##########################"
+echo "# Building site          #"
+echo "##########################"
 Rscript -e 'blogdown::build_site()'
 
-#echo "----------------"
-#echo "Syncing public/ to server..."
-#find . -name '.DS_Store' -delete
-#rsync -rltvz --delete public/ mercy:/srv/blog.jemu.name/public/
-
-echo "----------------"
-echo "Done with things"
-date +"%F %H:%M:%S"
-
+echo "##########################"
+echo "# Done! $(date +'%F %H:%M:%S')"
+echo "##########################"
