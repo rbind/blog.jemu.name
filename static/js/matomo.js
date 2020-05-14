a98714507737b00bd9290db0514c6bd4ce6ce90f
Lukas Burk <lukas@quantenbrot.de>
2020-05-14 21:54:43 +0200

    Add matomo via hardcoded params in .js

---
+++ static/js/matomo.js
@@ -0,0 +1,13 @@
+if (window.location.hostname != "127.0.0.1") {
+  var _paq = _paq || [];
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://matomo.jemu.name/";
+    _paq.push(["setTrackerUrl", u+"matomo.php"]);
+    _paq.push(["setSiteId", "2"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"matomo.js"; s.parentNode.insertBefore(g,s);
+  })();
+}
