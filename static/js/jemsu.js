9889fc8ec826e7e91194c8d8b7563a9547f1fbb8
Lukas Burk <lukas@quantenbrot.de>
2020-05-14 21:19:35 +0200

    colorscheme toggle works now I think?

--- static/js/jemsu.js
+++ static/js/jemsu.js
@@ -4,3 +4,27 @@ littlefoot.default({
   dismissDelay: 50,
   buttonTemplate: "<button aria-controls='fncontent:<% id %>' aria-expanded='false' aria-label='Footnote <% number %>' class='littlefoot-footnote__button' id='<% reference %>' rel='footnote' title='See Footnote <% number %>' /><% number %></button>"
 })
+
+// Color theme toggler via zookee1 <3
+function toggleDarkMode() {
+  let bodyClassList = document.body.classList;
+  let userPreferredIsLight = (window.matchMedia('(prefers-color-scheme: light)').matches)
+  
+  if(!bodyClassList.contains('colorscheme-dark') && 
+     !bodyClassList.contains('colorscheme-light')) {
+      bodyClassList.remove("colorscheme-auto");
+      if(userPreferredIsLight) {
+          bodyClassList.add("colorscheme-dark");            
+      } else {
+          bodyClassList.add("colorscheme-light");            
+      }
+  } else {
+      if(bodyClassList.contains('colorscheme-dark')) {
+          bodyClassList.add("colorscheme-light");
+          bodyClassList.remove("colorscheme-dark");
+      } else if(bodyClassList.contains('colorscheme-light')) {
+          bodyClassList.add("colorscheme-dark");
+          bodyClassList.remove("colorscheme-light");
+      }
+  }
+}
