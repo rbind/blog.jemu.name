c73ae85ae1351db9f24990865a7fd23517cad639
Lukas Burk <lukas@quantenbrot.de>
2020-05-22 17:29:18 +0200

    Make button work only if there's a toc

--- static/js/jemsu.js
+++ static/js/jemsu.js
@@ -32,24 +32,28 @@ function toggleDarkMode() {
 // Scroll to top thing: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 //Get the button:
-mybutton = document.getElementById("totop");
+scrollbutton = document.getElementById("totop");
 
-// When the user scrolls down 1000px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+if(typeof(scrollbutton) != 'undefined' && scrollbutton != null){
 
-function scrollFunction() {
-  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-    mybutton.style.visibility = "visible";
-    mybutton.style.opacity = "1";
-  } else {
-    mybutton.style.visibility = "hidden";
-    mybutton.style.opacity = "0";
+    // When the user scrolls down 1000px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
 
-  }
-}
+    function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        scrollbutton.style.visibility = "visible";
+        scrollbutton.style.opacity = "1";
+    } else {
+        scrollbutton.style.visibility = "hidden";
+        scrollbutton.style.opacity = "0";
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
 }
