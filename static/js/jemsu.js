1aebd37d99a55acecf6e4de74d9455e568a5a2a5
Lukas Burk <lukas@quantenbrot.de>
2020-05-22 16:42:34 +0200

    Add scroll-to-top thingy

--- static/js/jemsu.js
+++ static/js/jemsu.js
@@ -28,3 +28,25 @@ function toggleDarkMode() {
       }
   }
 }
+
+// Scroll to top thing: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+//Get the button:
+mybutton = document.getElementById("totop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
