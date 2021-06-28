d98060c479fa61bedecd421f0ecaa239fa45144e
Lukas Burk <lukas@quantenbrot.de>
2021-06-28 11:40:43 +0200

    Tweak littlefoot buttons

--- static/js/jemsu.js
+++ static/js/jemsu.js
@@ -2,21 +2,21 @@
 littlefoot.default({
   activateOnHover: false,
   dismissDelay: 50,
-  buttonTemplate: "<button aria-controls='fncontent:<% id %>' aria-expanded='false' aria-label='Footnote <% number %>' class='littlefoot-footnote__button' id='<% reference %>' rel='footnote' title='See Footnote <% number %>' /><% number %></button>"
+  buttonTemplate: '<button aria-label="Footnote <% number %>" class="littlefoot__button" id="<% reference %>" title="See Footnote <% number %>" /> <% number %> </button>'
 })
 
 // Color theme toggler via zookee1 <3
 function toggleDarkMode() {
   let bodyClassList = document.body.classList;
   let userPreferredIsLight = (window.matchMedia('(prefers-color-scheme: light)').matches)
-  
-  if(!bodyClassList.contains('colorscheme-dark') && 
+
+  if(!bodyClassList.contains('colorscheme-dark') &&
      !bodyClassList.contains('colorscheme-light')) {
       bodyClassList.remove("colorscheme-auto");
       if(userPreferredIsLight) {
-          bodyClassList.add("colorscheme-dark");            
+          bodyClassList.add("colorscheme-dark");
       } else {
-          bodyClassList.add("colorscheme-light");            
+          bodyClassList.add("colorscheme-light");
       }
   } else {
       if(bodyClassList.contains('colorscheme-dark')) {
