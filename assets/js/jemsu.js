f09fd7cde799ba8a3269b363863def48f81e404c
Lukas Burk <lukas@quantenbrot.de>
2025-03-08 15:13:17 +0100

    More cleanup

--- assets/js/jemsu.js
+++ assets/js/jemsu.js
@@ -2,35 +2,6 @@
 littlefoot.default({
     activateOnHover: false,
     dismissDelay: 50,
-    buttonTemplate: '<button aria-label="Footnote <% number %>" class="littlefoot__button" id="<% reference %>" title="See Footnote <% number %>" /> <% number %> </button>'
+    // buttonTemplate: '<button aria-label="Footnote <% number %>" class="littlefoot__button" id="<% reference %>" title="See Footnote <% number %>" /> <% number %> </button>'
+    buttonTemplate: '<button class="littlefoot__button" id="<% reference %>" title="See Footnote <% number %>" data-footnote-id="<% number %>"><% number %></button>'
 })
-
-
-// Scroll to top thing: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-
-//Get the button:
-scrollbutton = document.getElementById("totop");
-
-if (typeof (scrollbutton) != 'undefined' && scrollbutton != null) {
-
-    // When the user scrolls down 1000px from the top of the document, show the button
-    window.onscroll = function () { scrollFunction() };
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-            scrollbutton.style.visibility = "visible";
-            scrollbutton.style.opacity = "1";
-        } else {
-            scrollbutton.style.visibility = "hidden";
-            scrollbutton.style.opacity = "0";
-
-        }
-    }
-
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
-
-}
